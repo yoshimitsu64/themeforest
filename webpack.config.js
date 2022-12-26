a07af61071f433a3f751488fd40932847134fae3
yoshimitsu64 <yoshiyoshimitsu64@gmail.com>
2022-12-26 13:01:12 +0300

    fix: package.json

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  resolve: {
+    alias: {
+      globalStyles: path.resolve(__dirname, './src/globalStyles'),
+      pages: path.resolve(__dirname, './src/pages'),
+      components: path.resolve(__dirname, './src/components'),
+      constants: path.resolve(__dirname, './src/constants'),
+      assets: path.resolve(__dirname, './src/assets'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      helpers: path.resolve(__dirname, './src/helpers'),
+      appTypes: path.resolve(__dirname, './src/appTypes'),
+    },
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      favicon: path.resolve(__dirname, './public/favicon.png'),
+    }),
+    new Dotenv(),
+  ],
+};
