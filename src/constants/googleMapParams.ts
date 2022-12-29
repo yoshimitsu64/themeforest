322ed32068c57efe88bc1064261267164b2eafdc
yoshimitsu64 <yoshiyoshimitsu64@gmail.com>
2022-12-29 04:23:47 +0300

    feat: add interactive map

---
+++ src/constants/googleMapParams.ts
@@ -0,0 +1,19 @@
+export const containerStyle = {
+  width: '100%',
+  height: '460px',
+};
+
+export const coordinates = {
+  belarus: {
+    lat: 53.9006,
+    lng: 27.559,
+  },
+  poland: {
+    lat: 51.9194,
+    lng: 19.1451,
+  },
+  germany: {
+    lat: 51.1657,
+    lng: 10.4515,
+  },
+};
