6f7cd3cc3b15c4014a96fc56866017db5480b323
yoshimitsu64 <yoshiyoshimitsu64@gmail.com>
2023-01-03 08:06:02 +0300

    feat: add about us page

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -17,7 +17,7 @@ export const theme = {
   sizes: [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 820],
   borders: {
     borderRadius: [5, 10, 15, 20, 25, 30],
-    border: ['1px solid white'],
+    border: ['1px solid white', '2px solid black'],
   },
   fonts: [6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54],
   buttons: {
